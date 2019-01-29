9b962727e643171b5872d72ff87aa269ce22c73d
halkano <halkanoh10@gmaile.com>
2019-01-29 09:24:48 +0300

    added a defination for delete  data

--- user/user.py
+++ user/user.py
@@ -18,9 +18,14 @@ class User:
          user_list = []  # Empty user list
 
       # Init method up here
-      def save_user(self):
+    def save_user(self):
         '''
          save_user method saves user objects into user_list
          '''
 
         User.user_list.append(self)
+    def delete_user(self):
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+        User.user_list.remove(self)
