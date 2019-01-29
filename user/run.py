91c9399556acddad8406aab9808c00da97f32bcc
halkano <halkanoh10@gmaile.com>
2019-01-29 09:59:03 +0300

    imported creadentilas into run.py

---
+++ user/run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.6
+
+from credential import Credential
+def create_credential(fname,lname,password,email):
+    '''
+    Function to create a new credential
+    '''
+    new_credential = Credential(fname,lname,password,email)
+    return new_credential
