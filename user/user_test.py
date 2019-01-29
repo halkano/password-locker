9458547034fd5a45cb5cb5d874f07af7eef849c6
halkano <halkanoh10@gmaile.com>
2019-01-29 09:35:16 +0300

    added defination test to save users

--- user/user_test.py
+++ user/user_test.py
@@ -28,3 +28,10 @@ def test_init(self):
         self.assertEqual(self.new_user.last_name, "b")
         self.assertEqual(self.new_user.password, "c")
         self.assertEqual(self.new_user.email, "d")
+def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user()  # saving the new user
+        self.assertEqual(len(User.user_list), 1)
