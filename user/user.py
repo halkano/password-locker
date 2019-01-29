da4443e144cbbf0f07db041502133c4796430020
halkano <halkanoh10@gmaile.com>
2019-01-29 09:23:46 +0300

    added a defination for saving data

--- user/user.py
+++ user/user.py
@@ -18,3 +18,9 @@ class User:
          user_list = []  # Empty user list
 
       # Init method up here
+      def save_user(self):
+        '''
+         save_user method saves user objects into user_list
+         '''
+
+        User.user_list.append(self)
