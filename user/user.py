a99ad1ff92430e3ee2f8a7b394a060825f94cd96
halkano <halkanoh10@gmaile.com>
2019-01-29 09:22:03 +0300

    added a initialiazation

---
+++ user/user.py
@@ -0,0 +1,20 @@
+import pyperclip
+class User:
+    """
+   Class that generates new instances of user.
+     """
+
+    user_list = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,password,email):
+
+    # docstring removed for simplicity
+
+         self.first_name = first_name
+         self.last_name = last_name
+         self.password = password
+         self.email = email
+
+         user_list = []  # Empty user list
+
+      # Init method up here
