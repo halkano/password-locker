7cecae27ce57bb29403b985297d36bc570cbe86a
halkano <halkanoh10@gmaile.com>
2019-01-29 09:49:14 +0300

    defined and initialized the attributes

--- user/credential.py
+++ user/credential.py
@@ -5,3 +5,15 @@ class Credential:
         """
 
         credential_list = [] # Empty credential list
+    def __init__(self,first_name,last_name,password,email):
+
+          # docstring removed for simplicity
+
+            self.first_name = first_name
+            self.last_name = last_name
+            self.password = password
+            self.email = email
+
+        user_list = []  # Empty user list
+
+        # Init method up here
