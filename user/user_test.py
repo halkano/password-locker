a6e832d1841a95bd849d3e62f1e1d6bc28b88379
halkano <halkanoh10@gmaile.com>
2019-01-29 09:39:57 +0300

    added defination test to save teardown users

--- user/user_test.py
+++ user/user_test.py
@@ -74,3 +74,16 @@ def tearDown(self):
 
         self.new_user.delete_user()  # Deleting a user object
         self.assertEqual(len(User.user_list), 1)
+
+        def test_find_user_by_first_name(self):
+        '''
+        test to check if we can find a user by first_name and display information
+        '''
+
+        self.new_user.save_user()
+        test_user = User("Test", "user", "c", "d")  # new user
+        test_user.save_user()
+
+        found_user = User.find_by_first_name("Test")
+
+        self.assertEqual(found_user.email, test_user.email)
