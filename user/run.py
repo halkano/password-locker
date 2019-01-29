d4d15f6245eca4a141779297bd9e525f3e34f9c1
halkano <halkanoh10@gmaile.com>
2019-01-29 10:00:01 +0300

    created a save creadentilas into run.py

--- user/run.py
+++ user/run.py
@@ -7,3 +7,8 @@ def create_credential(fname,lname,password,email):
     '''
     new_credential = Credential(fname,lname,password,email)
     return new_credential
+    def save_credential(credential):
+    '''
+    Function to save credential
+    '''
+    Credential.save_credentials(credential
