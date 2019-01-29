0c007ff531c6f25b0cf02ea98863d89adcc495b6
halkano <halkanoh10@gmaile.com>
2019-01-29 09:34:25 +0300

    added defination setup for users

--- user/user_test.py
+++ user/user_test.py
@@ -19,3 +19,12 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("a", "b", "c", "d")  # create user object
+def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name, "a")
+        self.assertEqual(self.new_user.last_name, "b")
+        self.assertEqual(self.new_user.password, "c")
+        self.assertEqual(self.new_user.email, "d")
