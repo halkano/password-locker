35c9b7c659a1c8b6ba46268dc518da38eb7f1772
halkano <halkanoh10@gmaile.com>
2019-01-29 09:32:05 +0300

    added a class for the   the attributes

---
+++ user/user_test.py
@@ -0,0 +1,15 @@
+import unittest # Importing the unittest module
+import pyperclip, sys
+from user import User # Importing the user class
+
+class TestUser(unittest.TestCase):
+
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+    # Items up here ......
